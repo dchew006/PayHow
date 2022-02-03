860eb1a2a0a4cf728da9da3f7f1b0b393f6958e2
danielchew <danielchew66@gmail.com>
2022-02-03 20:18:16 +0800

    hope this works on heroku

--- test.py
+++ test.py
@@ -1,21 +1,37 @@
-from telegram.ext import Updater, CommandHandler, callbackcontext
-from telegram import Update
+import telebot
+from telegram.ext import Updater, commandhandler, messagehandler, filters
+import os
 
-from testconfig import TOKEN, PORT
+TOKEN = telebot.TeleBot("5038866075:AAEIhqiNHq6HTq_3KWgGq8cRe1XacK9mPmk")
 
+def start(update, context):
+    yourname = update.message.chat.first_name
+    msg ="Hi" + yourname+"! Welcome to the mimic bot!"
+    context.bot.send_message(update.message.chat.id, msg)
 
-def start(update:Update, context:callbackcontext):
-    update.message.reply_text("NIHAO {}".format(update.message.from_user.username))
+def mimic(update, context):
+    context.bot.send_message(update.bot.message.chat.id, update.message.text)
 
+def details(update, context):
+    context.bot.send_message(update.message.chat.id, update.message)
 
+def error(update, context):
+    context.bot.send_message(update.message.chat.id, "Oops error encountered!")
 
-if __name__ == '__main__':
-    updater = Updater(TOKEN)
-
+def main():
+    updater = Updater(token=TOKEN)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(commandhandler("/start", start))
+    dp.add_handler(messagehandler(filter.text, mimic))
+    dp.add_handler(commandhandler("/details", details))
+    dp.add_handler(error)
 
-    updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url='https://payhowtest.herokuapp.com'+TOKEN)
+    #WEBHOOK
+    updater.start_webhook(listen="0.0.0.0", port=os.environ.get("PORT", 443),
+                          url_path=TOKEN,
+                          webhook_url="https://payhowtest.herokuapp.com"+TOKEN)
     updater.idle()
 
+if __name__ == "__main__":
+    main()
