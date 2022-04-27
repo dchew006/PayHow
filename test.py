4eaf309d14de99c24fa7b9969dae750210ac959d
danielchew <danielchew66@gmail.com>
2022-04-27 23:28:09 +0800

    commit 1

--- test.py
+++ test.py
@@ -45,6 +45,6 @@ def webhook():
     bot.set_webhook(url='https://payhowtest.herokuapp.com/' + TOKEN)
     return "!", 200
 
-
+#
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
